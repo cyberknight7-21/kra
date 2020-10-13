4c5b1640fcb8ab2c3f6e455683cbe4d5b014d0ca
cyberknight7-21 <71084843+cyberknight7-21@users.noreply.github.com>
2020-10-13 12:02:23 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,181 +1,163 @@
-$(document).ready(function(){
-  // Initialize Firebase
-    var firebaseConfig = {
-    apiKey: "AIzaSyAn4SHwDb3odEvsVCA5HImagwMAP8zOW9o",
-    authDomain: "charged-ridge-234713.firebaseapp.com",
-    databaseURL: "https://charged-ridge-234713.firebaseio.com",
-    projectId: "charged-ridge-234713",
-    storageBucket: "charged-ridge-234713.appspot.com",
-    messagingSenderId: "226231639060",
-    appId: "1:226231639060:web:62c6bd0fc22d57dcb79b01",
-    measurementId: "G-34WR5SVPMW"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+(function() {
+  'use strict';
+  var lastId = 0;
+  var taskWrapper = document.getElementById("task_wrapper");
+  var btnSave = document.getElementById("save_task");
+  var removeIcon;
+  var updateIcon;
+  var taskList;
 
-  //create firebase references
-  var Auth = firebase.auth(); 
-  var dbRef = firebase.database();
-  var contactsRef = dbRef.ref('contacts')
-  var usersRef = dbRef.ref('users')
-  var auth = null;
+  // Initialize taskList 
+  // Add event to save button
+  // Render the list
 
-  //Register
-  $('#registerForm').on('submit', function (e) {
-    e.preventDefault();
-    $('#registerModal').modal('hide');
-    $('#messageModalLabel').html(spanText('<i class="fa fa-cog fa-spin"></i>', ['center', 'info']));
-    $('#messageModal').modal('show');
-    var data = {
-      email: $('#registerEmail').val(), //get the email from Form
-      firstName: $('#registerFirstName').val(), // get firstName
-      lastName: $('#registerLastName').val(), // get lastName
+  function init() {
+
+    if (!!(window.localStorage.getItem('taskList'))) {
+      taskList = JSON.parse(window.localStorage.getItem('taskList'));
+    } else {
+      taskList = [];
+    }
+    btnSave.addEventListener('click', saveTask);
+    showList();
+  }
+
+  //End Init
+
+  //CRUD task
+
+  function showList() {
+
+    if (!!taskList.length) {
+      getLastTaskId();
+      for (var item in taskList) {
+        var task = taskList[item];
+        addTaskToList(task);
+      }
+      syncEvents();
+    }
+    
+  }
+
+  function saveTask(event) {
+
+    var task = {
+      taskId: lastId,
+      taskDes: document.getElementById("task_des").value,
+      taskState: document.getElementById("task_state").value
     };
-    var passwords = {
-      password : $('#registerPassword').val(), //get the pass from Form
-      cPassword : $('#registerConfirmPassword').val(), //get the confirmPass from Form
+    taskList.push(task);
+    syncTask();
+    addTaskToList(task);
+    syncEvents();
+    lastId++;
+  }
+
+  function addTaskToList(task) {
+
+    var removeIcon = document.createElement('span');
+    var element = document.createElement('li');
+    var updateIcon = document.createElement('span');
+
+    removeIcon.innerHTML = "X";
+    removeIcon.className = "remove_item clickeable";
+    removeIcon.setAttribute("title", "Remove");
+
+    updateIcon.innerHTML = "U";
+    updateIcon.className = "update_icon clickeable";
+    updateIcon.setAttribute("title", "Update");
+
+
+    element.appendChild(removeIcon);
+    element.appendChild(updateIcon);
+    element.setAttribute("id", task.taskId);
+    element.innerHTML += task.taskDes;
+    taskWrapper.appendChild(element);
+  }
+
+  function updateTask(event) {
+
+    var taskTag = event.currentTarget.parentNode;
+    var taskId = taskTag.id;
+    var taskToUpdate = findTask(taskId).task;
+    var pos = findTask(taskId).pos;
+    if (!!taskToUpdate) {
+      var des = prompt("Task Description", taskToUpdate.taskDes);
+      var state = prompt("Task State", taskToUpdate.taskState);
+      taskToUpdate.taskDes = des;
+      taskToUpdate.taskState = state;
+      taskList[pos] = taskToUpdate;
+      taskTag.lastChild.textContent = taskToUpdate.taskDes;
+      syncTask();
     }
-    if( data.email != '' && passwords.password != ''  && passwords.cPassword != '' ){
-      if( passwords.password == passwords.cPassword ){
-        //create the user
-        
-        firebase.auth()
-          .createUserWithEmailAndPassword(data.email, passwords.password)
-          .then(function(user) {
-            return user.updateProfile({
-              displayName: data.firstName + ' ' + data.lastName
-            })
-          })
-          .then(function(user){
-            //now user is needed to be logged in to save data
-            auth = user;
-            //now saving the profile data
-            usersRef.child(user.uid).set(data)
-              .then(function(){
-                console.log("User Information Saved:", user.uid);
-              })
-            $('#messageModalLabel').html(spanText('Success!', ['center', 'success']))
-            
-            $('#messageModal').modal('hide');
-          })
-          .catch(function(error){
-            console.log("Error creating user:", error);
-            $('#messageModalLabel').html(spanText('ERROR: '+error.code, ['danger']))
-          });
-      } else {
-        //password and confirm password didn't match
-        $('#messageModalLabel').html(spanText("ERROR: Passwords didn't match", ['danger']))
+  }
+
+  function removeTask(event) {
+
+    var taskToRemove = event.currentTarget.parentNode;
+    var taskId = taskToRemove.id;
+    taskWrapper.removeChild(taskToRemove);
+    taskList.forEach(function(value, i) {
+      if (value.taskId == taskId) {
+        taskList.splice(i, 1);
       }
-    }  
-  });
+    })
 
-  //Login
-  $('#loginForm').on('submit', function (e) {
-    e.preventDefault();
-    $('#loginModal').modal('hide');
-    $('#messageModalLabel').html(spanText('<i class="fa fa-cog fa-spin"></i>', ['center', 'info']));
-    $('#messageModal').modal('show');
+    syncTask();
+  }
 
-    if( $('#loginEmail').val() != '' && $('#loginPassword').val() != '' ){
-      //login the user
-      var data = {
-        email: $('#loginEmail').val(),
-        password: $('#loginPassword').val()
-      };
-      firebase.auth().signInWithEmailAndPassword(data.email, data.password)
-        .then(function(authData) {
-          auth = authData;
-          $('#messageModalLabel').html(spanText('Success!', ['center', 'success']))
-          $('#messageModal').modal('hide');
-        })
-        .catch(function(error) {
-          console.log("Login Failed!", error);
-          $('#messageModalLabel').html(spanText('ERROR: '+error.code, ['danger']))
-        });
-    }
-  });
+  // End CRUD
 
-  $('#logout').on('click', function(e) {
-    e.preventDefault();
-    firebase.auth().signOut()
-  });
 
-  //save contact
-  $('#contactForm').on('submit', function( event ) {  
-    event.preventDefault();
-    if( auth != null ){
-      if( $('#name').val() != '' || $('#email').val() != '' ){
-        contactsRef.child(auth.uid)
-          .push({
-            name: $('#name').val(),
-            email: $('#email').val(),
-            location: {
-              city: $('#city').val(),
-              state: $('#state').val(),
-              zip: $('#zip').val()
-            }
-          })
-          document.contactForm.reset();
-      } else {
-        alert('Please fill at-lease name or email!');
+  //Common
+
+  function syncTask() {
+
+    window.localStorage.setItem('taskList', JSON.stringify(taskList));
+    taskList = JSON.parse(window.localStorage.getItem('taskList'));
+  }
+
+  function getLastTaskId() {
+    var lastTask = taskList[taskList.length - 1];
+    lastId = lastTask.taskId + 1;
+  }
+
+  function syncEvents() {
+
+    updateIcon = document.getElementsByClassName("update_icon");
+    removeIcon = document.getElementsByClassName("remove_item");
+    if (!!removeIcon.length) {
+      for (var i = 0; i < removeIcon.length; i++) {
+        removeIcon[i].addEventListener('click', removeTask);
       }
-    } else {
-      //inform user to login
     }
-  });
-
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      auth = user;
-      $('body').removeClass('auth-false').addClass('auth-true');
-      usersRef.child(user.uid).once('value').then(function (data) {
-        var info = data.val();
-        if(user.photoUrl) {
-          $('.user-info img').show();
-          $('.user-info img').attr('src', user.photoUrl);
-          $('.user-info .user-name').hide();
-        } else if(user.displayName) {
-          $('.user-info img').hide();
-          $('.user-info').append('<span class="user-name">'+user.displayName+'</span>');
-        } else if(info.firstName) {
-          $('.user-info img').hide();
-          $('.user-info').append('<span class="user-name">'+info.firstName+'</span>');
-        }
-      });
-      contactsRef.child(user.uid).on('child_added', onChildAdd);
-    } else {
-      // No user is signed in.
-      $('body').removeClass('auth-true').addClass('auth-false');
-      auth && contactsRef.child(auth.uid).off('child_added', onChildAdd);
-      $('#contacts').html('');
-      auth = null;
+    if (!!updateIcon.length) {
+      for (var j = 0; j < updateIcon.length; j++) {
+        updateIcon[j].addEventListener('click', updateTask);
+      }
     }
-  });
-});
+  }
 
-function onChildAdd (snap) {
-  $('#contacts').append(contactHtmlFromObject(snap.key, snap.val()));
-}
- 
-//prepare contact object's HTML
-function contactHtmlFromObject(key, contact){
-  return '<div class="card contact" style="width: 18rem;" id="'+key+'">'
-    + '<div class="card-body">'
-      + '<h5 class="card-title">'+contact.name+'</h5>'
-      + '<h6 class="card-subtitle mb-2 text-muted">'+contact.email+'</h6>'
-      + '<p class="card-text" title="' + contact.location.zip+'">'
-        + contact.location.city + ', '
-        + contact.location.state
-      + '</p>'
-      // + '<a href="#" class="card-link">Card link</a>'
-      // + '<a href="#" class="card-link">Another link</a>'
-    + '</div>'
-  + '</div>';
-}
+  function findTask(id) {
 
-function spanText(textStr, textClasses) {
-  var classNames = textClasses.map(c => 'text-'+c).join(' ');
-  return '<span class="'+classNames+'">'+ textStr + '</span>';
-}
+    var response = {
+      task: '',
+      pos: 0
+    };
+    taskList.forEach(function(value, i) {
+      if (value.taskId == id) {
+        response.task = value;
+        response.pos = i;
+      }
+    });
 
+    return response;
+  }
+
+  //End Common
+
+
+  init();
+
+
+})();
